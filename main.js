4ccaa493ada28a0661ccf7f65d7b7553617b3e9b
yasirlashari <yasirlashari131@gmail.com>
2024-05-01 14:19:32 +0500

    let can reassign

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-/*var was added is peviously now mostly let and const is used
- var is global scope  and let and const are block scope*/
-// let and const
-/*let a=2;
-console.log(a);
-let b=3;
-console.log(b);
-// here it will give no error
-*/
-const x=10;
-console.log(x);// const are variable that will be same always
-
-
-
+let age =20;
+age=23;
+console.log(age)
+// it will not give error bcz let can reassign

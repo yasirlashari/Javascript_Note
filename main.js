1aabe023e9242d2094b42681d51e3cb8b2f2fcbf
yasirlashari <yasirlashari131@gmail.com>
2024-05-01 14:17:43 +0500

    const example

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 /*var was added is peviously now mostly let and const is used
  var is global scope  and let and const are block scope*/
 // let and const
-let a=2;
+/*let a=2;
 console.log(a);
-let a=3;
-console.log(a);
-// here we can con not assign same a 
+let b=3;
+console.log(b);
+// here it will give no error
+*/
+const x=10;
+console.log(x);// const are variable that will be same always
+
 
 

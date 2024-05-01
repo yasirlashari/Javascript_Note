c629bb1daf564679cd1c8d9f612540a14e696565
yasirlashari <yasirlashari131@gmail.com>
2024-05-01 14:22:38 +0500

    Data types of js

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-let age =20;
-age=23;
-console.log(age)
-// it will not give error bcz let can reassign
-
-const score =3;
-score =5;
-console.log(score);
-//it will give error bcz const can not reassign 
+// Data types of js
+// string , number , boolen, null , undefined

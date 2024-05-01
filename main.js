0a9cbced3d65569296dad69bcff3ceaea50e6abc
yasirlashari <yasirlashari131@gmail.com>
2024-05-01 14:20:18 +0500

    const can not reassign

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ let age =20;
 age=23;
 console.log(age)
 // it will not give error bcz let can reassign
+
+const score =3;
+score =5;
+console.log(score);
+//it will give error bcz const can not reassign 

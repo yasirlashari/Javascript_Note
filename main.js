23bcc425a58cc3ebda7b8b557dbea2d4f559dec0
yasirlashari <yasirlashari131@gmail.com>
2024-05-01 14:15:54 +0500

    let example

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 /*var was added is peviously now mostly let and const is used
-* var is global scope  and let and const are block scope/
+ var is global scope  and let and const are block scope*/
 // let and const
+let a=2;
+console.log(a);
+let a=3;
+console.log(a);
+// here we can con not assign same a 
+
 
